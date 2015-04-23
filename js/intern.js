2e2c0103133b298eeb525b8427913a1c6b527c29
Kurt Medley <kurtmedley@kurt-medleys-mac-pro.local>
2015-04-22 18:33:40 -0700

    just boilerplate stuff

---
+++ js/intern.js
@@ -0,0 +1,5 @@
+// This is where you'll be writing your code. The code in here will execute
+// as soon as its rendered in your browser. This might cause a
+// problem if the document object model (DOM) hasn't built the elements you want
+// to manipulate. We need to add a 'ready' function to execute code when the
+// DOM is ready to be manipulated.
--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html', {root: __dirname});
 });
 
+app.get('/intern', function (req, res) {
+  res.sendFile('views/intern.html', {root: __dirname});
+});
+
 app.listen(3000, function () {
   console.log('Costco Internship server listening at http://127.0.0.1:3000');
 });
